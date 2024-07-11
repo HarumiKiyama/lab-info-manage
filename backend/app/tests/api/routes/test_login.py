dc88e466c8d004ec5c535ed69a2fbb440f242cb3
Harumi Kiyama <harumi@Harumis-MacBook-Air.local>
2024-07-11 19:43:13 +0800

    update

--- backend/app/tests/api/routes/test_login.py
+++ backend/app/tests/api/routes/test_login.py
@@ -1,5 +1,4 @@
 from fastapi.testclient import TestClient
-from pytest_mock import MockerFixture
 
 from app.core.config import settings
 
@@ -35,23 +34,3 @@ def test_use_access_token(
     result = r.json()
     assert r.status_code == 200
     assert "email" in result
-
-
-def test_recovery_password(
-    client: TestClient, normal_user_token_headers: dict[str, str], mocker: MockerFixture
-) -> None:
-    mocker.patch("app.utils.send_email", return_value=None)
-    mocker.patch("app.core.config.settings.SMTP_HOST", "smtp.example.com")
-    mocker.patch("app.core.config.settings.SMTP_USER", "admin@example.com")
-    email = "test@example.com"
-    r = client.post(
-        f"{settings.API_V1_STR}/password-recovery/{email}",
-        headers=normal_user_token_headers,
-    )
-    assert r.status_code == 200
-    assert r.json() == {"message": "Password recovery email sent"}
-
-
-
-
-
