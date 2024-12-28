b0948854dc86e74477e00062ce8717404398cfae
Your Name <you@example.com>
2024-12-28 23:45:47 +0800

    update

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, Relationship, SQLModel,Column,String
+from sqlmodel import Field, Relationship, SQLModel, Column, String
 
 
 
@@ -6,15 +6,17 @@ from sqlmodel import Field, Relationship, SQLModel,Column,String
 class UserBase(SQLModel):
     is_active: bool = True
     is_superuser: bool = False
-    email :str  = Field(index=True)
+    email: str = Field(index=True)
 
 
 # Properties to receive via API on creation
 class UserCreate(UserBase):
     password: str
-    email :str  
+    email: str
     username: str
-    full_name:str | None = None
+    full_name: str | None = None
+
+
 class UserCreateOpen(SQLModel):
     phonenum: str
     username: str
@@ -25,13 +27,14 @@ class UserCreateOpen(SQLModel):
 class UserUpdate(UserBase):
     phonenum: str | None = None  # type: ignore
     password: str | None = None
-    full_name:str | None = None
+    full_name: str | None = None
+
 
 class UserUpdateMe(SQLModel):
     full_name: str | None = None
     phonenum: str | None = None
-    email: str |None = None
-    
+    email: str | None = None
+
 
 class UpdatePassword(SQLModel):
     current_password: str
@@ -42,16 +45,15 @@ class UpdatePassword(SQLModel):
 class User(UserBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     hashed_password: str
-    username:str 
+    username: str
     full_name: str | None = Field(default=None, nullable=True)
-    
-    
+
 
 # Properties to return via API, id is always required
 class UserOut(UserBase):
     id: int
-    full_name:str  | None = None
-    
+    full_name: str | None = None
+
 
 class UsersOut(SQLModel):
     data: list[UserOut]
@@ -72,6 +74,7 @@ class TokenPayload(SQLModel):
 class NewPassword(SQLModel):
     token: str
     new_password: str
+
 
 class Message(SQLModel):
     pass
